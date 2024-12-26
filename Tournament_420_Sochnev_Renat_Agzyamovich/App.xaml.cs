1d6de40fbf36af00eed58e449da816931dbb6573
sakazukiakaakainu@gmail.com <sakazukiakaakainu@gmail.com>
2024-12-26 12:54:48 +0300

    Добавьте файлы проекта.

---
+++ Tournament_420_Sochnev_Renat_Agzyamovich/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Tournament_420_Sochnev_Renat_Agzyamovich
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
